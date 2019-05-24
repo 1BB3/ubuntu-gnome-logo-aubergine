e2e8b6db7e9690ca825faf394a9675d7b32e2697
Sanjeev Kc <sanjeev1BB3@protonmail.com>
2019-05-24 15:32:05 +0545

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo cp  -r ../ubuntu-gnome-logo-aubergine /usr/share/plymouth/themes/
 
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/ubuntu-gnome-logo-aubergine/ubuntu-gnome-logo.plymouth 100
+sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/ubuntu-gnome-logo-aubergine/ubuntu-gnome-logo-aubergine.plymouth 100
 
 sudo update-alternatives --config default.plymouth
 
